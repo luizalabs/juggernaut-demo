fb3db5e0e64f4beecbc5a6bd02fab8931261f49f
Marlon Maxwel <marlonmleite@gmail.com>
2019-08-28 10:25:31 -0300

    Remove {"

--- src/containers/SignInSide/index.jsx
+++ src/containers/SignInSide/index.jsx
@@ -66,7 +66,7 @@ function SignInSide() {
               </Grid>
               <Grid item>
                 <Link href="#" variant="body2">
-                  {"Don't have an account? Sign Up"}
+                  Don't have an account? Sign Up
                 </Link>
               </Grid>
             </Grid>
