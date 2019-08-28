a61790c3a656496822f13786b78ee84d4e1f33f8
Marlon Maxwel <marlonmaxwel@softbox.com.br>
2019-08-28 10:16:12 -0300

    Adicionando novo demo e melhorando apresentação da página inicial

---
+++ src/containers/Home/Copyright.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from '@material-ui/core'
+import { TypographyStyled, IconStyled } from './style'
+
+function Copyright() {
+  return (
+    <TypographyStyled variant="body2" color="textSecondary" align="center" className="my-3">
+      <Link color="inherit" href="https://softbox.com.br/">
+        Copyright © Softbox.
+      </Link>
+      <span className="ml-1">Built with</span>
+      <IconStyled width="16px" height="16px" />
+      <Link color="inherit" href="https://github.com/SoftboxLab/juggernaut">
+        by Juggernaut.
+      </Link>
+    </TypographyStyled>
+  );
+}
+
+export default Copyright
