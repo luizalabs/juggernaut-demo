d5b37dd188b55d487201eb5e667ad0eed1ab1053
thiagomiranda <thiagomiranda@softbox.com.br>
2019-08-30 12:07:13 -0300

    sign up container

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Grid, Typography, CardActions, Button, Container } from '@material-ui/core'
 import signIn from 'assets/img/sign-in.png'
 import signInSide from 'assets/img/sign-in-side.png'
+import signUp from 'assets/img/sign-up.png'
 import logo from 'assets/img/juggernaut.gif'
 import Copyright from './Copyright'
 import { GridItemStyled, CardStyled, CardMediaStyled, CardContentStyled, ImgStyled, HomeStyled } from './style'
@@ -20,6 +21,13 @@ const layouts = [
     src: signInSide,
     href: '/demo/sign-in-side/',
     source: 'https://github.com/SoftboxLab/juggernaut-demo/tree/master/src/containers/SignInSide'
+  },
+  {
+    title: 'Sign Up',
+    description: 'A simple Sign Up page.',
+    src: signUp,
+    href: '/demo/sign-up/',
+    source: 'https://github.com/SoftboxLab/juggernaut-demo/tree/master/src/containers/SignUp'
   }
 ]
 
