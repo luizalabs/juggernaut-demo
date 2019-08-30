d0cee46940a2e7a6e11638d6daf8ad00bd0ba3cf
thiagomiranda <thiagomiranda@softbox.com.br>
2019-08-30 12:07:13 -0300

    sign up container

---
+++ src/containers/SignUp/index.jsx
@@ -0,0 +1,109 @@
+import React from 'react'
+import Button from '@material-ui/core/Button'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import TextField from '@material-ui/core/TextField'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import Checkbox from '@material-ui/core/Checkbox'
+import Link from '@material-ui/core/Link'
+import Grid from '@material-ui/core/Grid'
+import Box from '@material-ui/core/Box'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
+import Typography from '@material-ui/core/Typography'
+import Container from '@material-ui/core/Container'
+import { FormContainerStyled, FormStyled, AvatarStyled } from './style'
+
+function SignUp() {
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <FormContainerStyled>
+        <AvatarStyled>
+          <LockOutlinedIcon />
+        </AvatarStyled>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        <FormStyled>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                autoComplete="fname"
+                name="firstName"
+                variant="outlined"
+                required
+                fullWidth
+                id="firstName"
+                label="First Name"
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="lastName"
+                label="Last Name"
+                name="lastName"
+                autoComplete="lname"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <FormControlLabel
+                control={<Checkbox value="allowExtraEmails" color="primary" />}
+                label="I want to receive inspiration, marketing promotions and updates via email."
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className="mt-3 mb-2 mx-0"
+          >
+            Sign Up
+          </Button>
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Link href="#" variant="body2">
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+        </FormStyled>
+      </FormContainerStyled>
+      <Box mt={5}>
+        <Typography variant="body2" color="textSecondary" align="center">
+          {'Juggernaut Demo - '}
+          {new Date().getFullYear()}
+        </Typography>
+      </Box>
+    </Container>
+  )
+}
+
+export default SignUp
