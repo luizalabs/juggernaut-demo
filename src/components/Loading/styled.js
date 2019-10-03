97d757c5b3620ac233a8c6975ca4c6cfc1ef1552
marco2120 <37886221+marco2120@users.noreply.github.com>
2019-10-03 10:43:33 -0300

    Definir altura minima para Device

--- src/components/Loading/styled.js
+++ src/components/Loading/styled.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 const LoadingStyled = styled.div`
   width: 100%;
   height: 100vh;
+  min-height: 100vh;
   display: flex;
   align-items: center;
   justify-content: center;
