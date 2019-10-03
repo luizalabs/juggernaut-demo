ae19f5a2e6fc95c300deab28a5f1538f6e49de3b
PabloAlves <pablogabrielcastroalves@gmail.com>
2019-10-03 14:04:44 -0300

    componente toolbar

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -4,6 +4,7 @@ import signIn from 'assets/img/sign-in.png'
 import signInSide from 'assets/img/sign-in-side.png'
 import signUp from 'assets/img/sign-up.png'
 import modal from 'assets/img/modal.png'
+import toolbar from 'assets/img/toolbar.png'
 import logo from 'assets/img/juggernaut.gif'
 import Copyright from './Copyright'
 import { GridItemStyled, CardStyled, CardMediaStyled, CardContentStyled, ImgStyled, HomeStyled } from './style'
@@ -36,6 +37,13 @@ const layouts = [
     src: modal,
     href: '/demo/modal/',
     source: 'https://github.com/SoftboxLab/juggernaut-demo/tree/master/src/containers/Modal'
+  },
+  {
+    title: 'Toolbar',
+    description: 'A simple Toolbar exmaple.',
+    src: toolbar,
+    href: '/demo/toolbar/',
+    source: 'https://github.com/SoftboxLab/juggernaut-demo/tree/master/src/containers/Toolbar'
   }
 ]
 
