192d5972a152b14c730d733ca2eda1bdd15d1098
Stanley <stanleygomes@softbox.com.br>
2019-10-07 14:46:45 -0300

    ajustes de projeto

--- src/routes/demo.js
+++ src/routes/demo.js
@@ -3,6 +3,7 @@ import { lazy } from 'react'
 const SignIn = lazy(() => import('containers/SignIn'))
 const SignInSide = lazy(() => import('containers/SignInSide'))
 const SignUp = lazy(() => import('containers/SignUp'))
+const Tabs = lazy(() => import('containers/Tabs'))
 
 const demo = [
   {
@@ -18,6 +19,11 @@ const demo = [
   {
     path: '/demo/sign-up',
     main: SignUp,
+    exact: true
+  },
+  {
+    path: '/demo/tabs',
+    main: Tabs,
     exact: true
   }
 ]
