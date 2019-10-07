2bded284e4a6169d66947a3415cd1143fac7e33b
Pablo Alves <46485192+PabloAlves@users.noreply.github.com>
2019-10-07 11:31:57 -0300

    Update index.jsx

--- src/containers/Menu/index.jsx
+++ src/containers/Menu/index.jsx
@@ -4,7 +4,7 @@ import Menu from '@material-ui/core/Menu'
 import MenuItem from '@material-ui/core/MenuItem'
 import { ContainerStyled } from './style'
 
-const SimpleMenu = () => {
+function SimpleMenu() {
   const [anchorEl, setAnchorEl] = React.useState(null);
 
   const handleClick = event => {
