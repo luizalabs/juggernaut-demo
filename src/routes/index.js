8d17ea9c2e4e1dc328b57efec22aeb10b0c406c5
Stanley <stanleygomes@softbox.com.br>
2019-10-07 14:40:30 -0300

    ajustes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,11 @@
 import home from './home'
 import demo from './demo'
 import modal from './modal'
-import table from './table'
 import toolbar from './toolbar'
 
 const routes = [
   ...home,
   ...demo,
-  ...table,
   ...modal,
   ...toolbar
 ]
