fd581f8682bab4e423a907ead627da8e0fcbcbc0
Stanley <stanleygomesdasilva@gmail.com>
2019-10-21 17:42:37 -0300

    Update index.jsx

--- src/containers/Table/index.jsx
+++ src/containers/Table/index.jsx
@@ -11,12 +11,12 @@ const createData = (name, calories, fat, carbs, protein) => {
 
 const rows = [
   createData('Iron Man', 5.5, 8.5, 4.5, 10.0),
-  createData('Capitain America', 2.5, 3.0, 8.0, 2.5),
+  createData('Captain America', 2.5, 3.0, 8.0, 2.5),
   createData('Black Widow S2', 3.5, 2.5, 4.0, 6.0),
   createData('Hulk', 7.0, 9.9, 9.9, 1.5),
   createData('Thanos (sem manopla)', 4.0, 9.5, 9.2, 8.5),
   createData('Goku', 8000, 8000, 8000, 8000),
-  createData('Capitain Marvel', 7.5, 8.5, 8.2, 2.5),
+  createData('Captain Marvel', 7.5, 8.5, 8.2, 2.5),
 ]
 
 function SimpleTable() {
@@ -28,8 +28,8 @@ function SimpleTable() {
             <TableRow>
               <TableCell>Personagem</TableCell>
               <TableCell align="right">Velocidade</TableCell>
-              <TableCell align="right">For&ccedil;a</TableCell>
-              <TableCell align="right">Resist&ecirc;ncia</TableCell>
+              <TableCell align="right">Força</TableCell>
+              <TableCell align="right">Resistência</TableCell>
               <TableCell align="right">Tecnologia</TableCell>
             </TableRow>
           </TableHead>
