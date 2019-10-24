e3f0708cb1a4dfa754690d701e20021a24ef776c
stanley <stanleygomesdasilva@gmail.com>
2019-10-24 20:19:02 -0300

    ajustes de importacao do componente de tabs

--- src/containers/Tabs/style.js
+++ src/containers/Tabs/style.js
@@ -1,15 +1,10 @@
 import styled from 'styled-components'
 import Paper from '@material-ui/core/Paper'
-import Tabs from '@material-ui/core/Tabs'
 
 const PaperStyled = styled(Paper)`
   flex-grow: 1;
 `
 
-const TabsStyled = styled(Tabs)`
-`
-
 export {
-  PaperStyled,
-  TabsStyled
+  PaperStyled
 }
