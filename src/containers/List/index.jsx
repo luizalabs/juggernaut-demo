b69769883405a18dd502684f4ba000303e36c6fa
stanley <stanleygomesdasilva@gmail.com>
2020-09-24 20:44:30 -0300

    a list example

---
+++ src/containers/List/index.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import Divider from '@material-ui/core/Divider'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ListItemText from '@material-ui/core/ListItemText'
+import InboxIcon from '@material-ui/icons/Inbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import { ContainerStyled } from './style'
+
+function SimpleList() {
+  return (
+    <ContainerStyled
+      container
+      alignItems="center"
+      justify="space-around"
+    >
+      <List component="nav" aria-label="main mailbox folders">
+        <ListItem button>
+          <ListItemIcon>
+            <InboxIcon />
+          </ListItemIcon>
+          <ListItemText primary="Inbox" />
+        </ListItem>
+        <ListItem button>
+          <ListItemIcon>
+            <DraftsIcon />
+          </ListItemIcon>
+          <ListItemText primary="Drafts" />
+        </ListItem>
+      </List>
+      <Divider />
+      <List component="nav" aria-label="secondary mailbox folders">
+        <ListItem button>
+          <ListItemText primary="Trash" />
+        </ListItem>
+        <ListItem button component="a" href="#simple-list">
+          <ListItemText primary="Spam" />
+        </ListItem >
+      </List>
+    </ContainerStyled>
+  )
+}
+
+export default SimpleList
