a070e8c5ad485ba44fa1b4c50c364bed9a715abd
Lucas <lucas@vilaboim.com>
2020-11-19 15:43:23 -0300

    feat(update-jug): add docs

---
+++ doczrc.js
@@ -0,0 +1,11 @@
+export default {
+  title: 'Juggernaut',
+  description: 'An unstoppable boilerplate',
+  ignore: ['README.md', 'CREATE_REACT_APP.md'],
+  files: 'docs/**/*.{md,markdown,mdx}',
+  themeConfig: {
+    colors: {
+      primary: 'tomato'
+    }
+  }
+}
